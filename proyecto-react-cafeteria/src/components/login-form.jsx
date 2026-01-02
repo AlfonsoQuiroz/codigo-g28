5caee879231e1c9ba4a4c3113a689e54a0497ab5
AlfonsoQuiroz <aquirozh_24@yahoo.es>
2026-01-02 02:42:10 -0500

    filtros

--- proyecto-react-cafeteria/src/components/login-form.jsx
+++ proyecto-react-cafeteria/src/components/login-form.jsx
@@ -23,9 +23,9 @@ export function LoginForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader>
-          <CardTitle>Login to your account</CardTitle>
+          <CardTitle>Ingresa con tu cuenta</CardTitle>
           <CardDescription>
-            Enter your email below to login to your account
+            Introduce tu correo electrónico a continuación para iniciar sesión en tu cuenta.{" "}
           </CardDescription>
         </CardHeader>
         <CardContent>
@@ -37,22 +37,22 @@ export function LoginForm({
               </Field>
               <Field>
                 <div className="flex items-center">
-                  <FieldLabel htmlFor="password">Password</FieldLabel>
-                  <a
+                  <FieldLabel htmlFor="password">Contraseña</FieldLabel>
+                  {/* <a
                     href="#"
                     className="ml-auto inline-block text-sm underline-offset-4 hover:underline">
                     Forgot your password?
-                  </a>
+                  </a> */}
                 </div>
                 <Input id="password" type="password" required />
               </Field>
               <Field>
-                <Button type="submit">Login</Button>
-                <Button variant="outline" type="button">
+                <Button type="submit">Iniciar Sesión</Button>
+                {/* <Button variant="outline" type="button">
                   Login with Google
-                </Button>
+                </Button> */}
                 <FieldDescription className="text-center">
-                  Don&apos;t have an account? <a href="#">Sign up</a>
+                  No tienes una cuenta? <a href="#">Registrate</a>
                 </FieldDescription>
               </Field>
             </FieldGroup>
